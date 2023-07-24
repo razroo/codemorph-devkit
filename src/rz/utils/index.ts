b017a498795e39b07cfc0967b98ed396f38bf5e6
Charlie Greenman <charlie@razroo.com>
2023-07-25 02:15:25 +0300

    remove broken index.

--- src/rz/utils/index.ts
+++ src/rz/utils/index.ts
@@ -1,5 +1,4 @@
 export { readPackageJson, determineLanguagesUsed } from "./package-json/package-json";
-export { findClosestModuleFile } from "./find-module-file/find-module-file";
 export { getVersionAndNameString } from "./razroo-path/razroo-path";
 export { determineType } from "./determine-type/determine-type";
 export { determineFilePathParameter } from "./determine-parameter/determine-parameter";
